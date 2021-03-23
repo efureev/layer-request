814dba60e1d81e5508b066836306505e40c4f5f5
Jack <furegin@yandex.ru>
2021-03-23 15:47:00 +0500

    almost stable version

--- tests/instance.js
+++ tests/instance.js
@@ -20,8 +20,8 @@ describe('create request by default', () => {
       assert.strictEqual(true, isEmpty(request.extra))
 
       assert.strictEqual(true, request.manager.all() instanceof Map)
-      assert.strictEqual(1, request.manager.list().length)
-      assert.strictEqual(true, request.manager.getLayer('/') instanceof ConfigLayer)
+      assert.strictEqual(0, request.manager.list().length)
+      assert.strictEqual(null, request.manager.getLayer('/'))
 
     })
 
